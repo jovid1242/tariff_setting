1926052085a5e784514cc43bab84c7ca28d7843c
jovid1242 <jovid1242@gmail.com>
2022-01-19 22:22:00 +0500

    fix server.js

--- server/server.js
+++ server/server.js
@@ -6,22 +6,23 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-var minute = 0;
-var sms = 0;
-var enternet = 0;
-var social = 0;
+const arr = [
+  { name: "minute", count: 0, price: 2 },
+  { name: "sms", count: 0, price: 1 },
+  { name: "enternet", count: 0, price: 25 },
+  { name: "social", count: 0, price: 15 },
+];
 
 app.post("/api/tariff_settings", (req, res) => {
-  if (req.body.type === "minute") {
-    minute = req.body.count * 2;
-  } else if (req.body.type === "sms") {
-    sms = req.body.count * 1;
-  } else if (req.body.type === "enternet") {
-    enternet = req.body.count * 25;
-  } else if (req.body.type === "social") {
-    social = req.body.count * 20;
-  }
-  res.json({ data: minute + sms + enternet + social });
+  arr.forEach((el) => {
+    if (req.body.type === el.name) {
+      el.count = req.body.count;
+    }
+  });
+  let sum = arr.reduce((a, b) => {
+    return a + b["price"] * b["count"];
+  }, 0);
+  res.json({ data: sum });
 });
 
 app.listen(port, () => {
