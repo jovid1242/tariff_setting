aeaadd9b6279baf214ab21a37524610bc03f0bbb
jovid1242 <jovid1242@gmail.com>
2022-01-19 15:53:40 +0500

    edit social icons

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -7,46 +7,26 @@ export default function Social() {
   const handleClick = (e) => {
     setactiveTab(e);
   };
+
+  const arrSocial = ["facebook", "instagram", "whatsapp", "telegram"];
   return (
     <div>
       <div className="social">
         <ul>
-          <li
-            className={activeTab === "facebook" ? "activeTab" : ""}
-            onClick={() => handleClick("facebook")}
-          >
-            <i
-              className="fab fa-facebook"
-              style={{ color: "#fff", width: "25px", height: "25px" }}
-            ></i>
-          </li>
-          <li
-            className={activeTab === "instagram" ? "activeTab" : ""}
-            onClick={() => handleClick("instagram")}
-          >
-            <i
-              className="fab fa-instagram"
-              style={{ color: "#fff", width: "25px", height: "25px" }}
-            ></i>
-          </li>
-          <li
-            className={activeTab === "telegram" ? "activeTab" : ""}
-            onClick={() => handleClick("telegram")}
-          >
-            <i
-              className="fab fa-telegram"
-              style={{ color: "#fff", width: "25px", height: "25px" }}
-            ></i>
-          </li>
-          <li
-            className={activeTab === "whatsapp" ? "activeTab" : ""}
-            onClick={() => handleClick("whatsapp")}
-          >
-            <i
-              className="fab fa-whatsapp"
-              style={{ color: "#fff", width: "25px", height: "25px" }}
-            ></i>
-          </li>
+          {arrSocial?.map((el, i) => {
+            return (
+              <li
+                className={activeTab === el ? "activeTab" : ""}
+                onClick={() => handleClick(el)}
+                key={i}
+              >
+                <i
+                  className={`fab fa-${el}`}
+                  style={{ color: "#fff", width: "25px", height: "25px" }}
+                ></i>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
